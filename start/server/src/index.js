b0da5f5c11bc75995d01ff9e1141170eb15500d2
Mohammed Al-Maqbli <Mohammed.almaqbli@gmail.com>
2021-05-15 07:39:25 +0300

    Create ApoloServer

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,7 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+import { ApolloServer } from "apollo-server";
+import typeDefs from "./schema";
+
+dotenv.config();
+
+const server = new ApolloServer({ typeDefs });
