34f8529d12b56992d6454b11249c9aa3187e711e
Mohammed Al-Maqbli <Mohammed.almaqbli@gmail.com>
2021-05-15 09:45:20 +0300

    Update index.js

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,27 @@
 import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./schema";
+import Post from "./datasources/post";
+import Launch from "./datasources/launch";
+import User from "./datasources/user";
+import { createStore } from "./utils";
 
 dotenv.config();
 
-const server = new ApolloServer({ typeDefs });
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    Post: new Post({ store }),
+    User: new User({ store }),
+    Launch: new Launch(),
+  }),
+});
+
+server.listen().then(() => {
+  console.log(`
+      Server is running!
+      Listening on port 4000
+    `);
+});
